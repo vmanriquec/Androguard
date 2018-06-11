4c5c3b732acbd0bb8cd2442e9a040a04728a9304
vmanriquec <pool_rodrigo1@yahoo.com>
2018-06-11 14:26:12 -0500

    ultima actualizacion

---
+++ app/src/main/java/com/space/sodapop/androguard/fragments/WelcomeFragment.java
@@ -0,0 +1,31 @@
+package com.space.sodapop.androguard.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.space.sodapop.androguard.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WelcomeFragment extends Fragment {
+
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+    }
+
+}
